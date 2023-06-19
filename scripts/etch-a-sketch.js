e9466d3a28ffff6362b4b1eaec12c2e5526e07da
rodolfoarambulo <108365768+rodolfoarambulo@users.noreply.github.com>
2023-06-19 19:54:59 +0800

    Fix grid logic to draw a 16x16 square grid

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,6 +1,6 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 256; i++) {
     let newDiv = document.createElement('div');
     newDiv.id = 'g' + i;
     newDiv.className = 'grid';
