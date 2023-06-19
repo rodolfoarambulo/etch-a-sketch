be230aba75ee20f10f394c7af18208c29efdca7c
rodolfoarambulo <108365768+rodolfoarambulo@users.noreply.github.com>
2023-06-19 20:59:48 +0800

    Add hover effect to grid elements

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -4,5 +4,10 @@ for (let i = 1; i <= 256; i++) {
     let newDiv = document.createElement('div');
     newDiv.id = 'g' + i;
     newDiv.className = 'grid';
+    newDiv.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'black';
+        },
+        false
+    );
     container.appendChild(newDiv);
 }
